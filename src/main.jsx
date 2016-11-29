a9355099f213a6288e23fd846dcb5b1275ca55e9
Sylvia <sylviad1221@gmail.com>
2016-11-29 16:55:51 -0500

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var NavBar = require('./components/nav/NavBar.jsx');
+
+var navLinks = [
+  {
+    title: "Home",
+    href: "#"
+  },
+  {
+    title: "Courses",
+    href: "#"
+  },
+  {
+    title: "Blog",
+    href: "#"
+  },
+];
+
+ReactDOM.render(<NavBar bgColor="#fff" titleColor="#3097d1" navData={navLinks}/>, document.getElementById('nav'));
